984e37c71c9efbf43224084dcbd46aaad1a4d972
Gustavo Siqueira <guss.ns@gmail.com>
2022-08-07 23:47:56 -0300

    first commit

---
+++ api-backend/APIBookstore/Models/todoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBookstore.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> option) : base(option) { }
+
+        public DbSet<Product> TodoProducts { get; set; }
+    }
+}
